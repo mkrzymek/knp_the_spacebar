242cb8384c87323136b05b2be56fe3a6bda260c2
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-08-24 13:48:38 +0200

    replaced deprecated Sensio\Bundle\FrameworkExtraBundle\Configuration\Route to Symfony\Component\Routing\Annotation\Route

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
