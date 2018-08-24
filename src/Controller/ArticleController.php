692097e6929637ec6552742b0cf6dadd57632358
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-08-24 14:05:12 +0200

    logger twig service without shortcut example

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
@@ -20,7 +22,10 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug)
+    public function show(
+        $slug
+//        , Environment $twigEnvironment
+    )
     {
         $comments = [
             'dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla',
@@ -29,6 +34,17 @@ class ArticleController extends AbstractController
         ];
 
 //        dump($slug,$this, $GLOBALS);
+
+        // Dont do that! :D
+//        $html = $twigEnvironment->render('article/show.html.twig',
+//            [
+//                'title' => ucwords(str_replace('-', ' ', $slug)),
+//                'slug' => $slug,
+//                'comments' => $comments
+//            ]);
+//
+//        return new Response($html);
+
 
         return $this->render('article/show.html.twig',
             [
@@ -41,11 +57,13 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart($slug)
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
     {
 //        TODO - actually heart/unheart the article
 
 //        return new JsonResponse(['hearts' => rand(5,100)]);
-        return $this->json(['hearts' => rand(5,100)]);
+        $logger->info('Article is being hearted');
+
+        return $this->json(['hearts' => rand(5, 100)]);
     }
 }
