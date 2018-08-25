41e4203e93cb298338592fdf9e378421c5be1ced
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-08-25 10:01:29 +0200

    monolog other channel service config

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
@@ -11,17 +12,24 @@ class MarkdownHelper
 
     private $markdown;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    private $logger;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $logger;
     }
 
-    public function parse(string $source) : string
+    public function parse(string $source): string
     {
-        $item = $this->cache->getItem('markdown_'.md5($source));
+        if (strip_tags($source, 'bacon') !== false) {
+            $this->logger->info('Bacon again!');
+        }
 
-        if(!$item->isHit()) {
+        $item = $this->cache->getItem('markdown_' . md5($source));
+
+        if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
             $this->cache->save($item);
         }
