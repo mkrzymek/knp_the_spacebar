09378c0b7b79660e4db1bda6287de754e16258d8
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-08-25 22:17:29 +0200

    Updating an entity with new fields, make:entity, make:migration, doctrine:migrations:migrate

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -40,6 +40,10 @@ EOF
             $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
         }
 
+        $article->setAuthor('David Beckham')
+            ->setHeartCount(rand(5,100))
+            ->setImageFilename('asteroid.jpeg');
+
         $em->persist($article);
         $em->flush();
 
