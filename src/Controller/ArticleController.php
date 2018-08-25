42294fe6259142f1fabc8d2a2330b1c33404a2c3
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-08-25 20:33:22 +0200

    quering for data twig markdown - no cache :(

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Service\MarkdownHelper;
 use App\Service\SlackClient;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Tests\Fixtures\includes\HotPath\P1;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -33,13 +36,21 @@ class ArticleController extends AbstractController
      */
     public function show(
         $slug,
-        MarkdownHelper $markdownHelper,
-        SlackClient $slack
+        SlackClient $slack,
+        EntityManagerInterface $em
 //        , Environment $twigEnvironment
     )
     {
         if ($slug == 'khaaaan') {
             $slack->sendMessage('Khan', 'Wysyłam do slaczka wiadomość');
+        }
+
+        $repository = $em->getRepository(Article::class);
+        /** @var Article $article */
+        $article = $repository->findOneBy(['slug' => $slug]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(sprintf('No article for slug "%s"', $slug));
         }
 
         $comments = [
@@ -48,33 +59,11 @@ class ArticleController extends AbstractController
             'deserunt mollit anim id est laborum.'
         ];
 
-        $articleContent = <<<EOF
-Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow, lorem proident beef ribs
-aute enim veniam ut cillum pork chuck **picanha**. Dolore reprehenderit labore minim pork belly spare ribs cupim short 
-loin in. [Elit exercitation](https://baconipsum.com/) eiusmod dolore cow turkey shank eu pork belly meatball non cupim.
-
-Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur laboris sunt 
-venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder, capicola biltong frankfurter boudin
-cupim officia. Exercitation fugiat consectetur ham. Adipisicing picanha shank et filet mignon pork belly ut ullamco
-. Irure velit turducken ground round doner incididunt occaecat lorem meatball prosciutto quis strip steak.
-
-Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak mollit quis 
-officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon strip steak pork belly 
-aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur cow est ribeye adipisicing. 
-Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck fugiat.
-EOF;
-
-        $articleContent = $markdownHelper->parse($articleContent);
-
         return $this->render('article/show.html.twig',
             [
-                'title' => ucwords(str_replace('-', ' ', $slug)),
-                'articleContent' => $articleContent,
-                'slug' => $slug,
+                'article' => $article,
                 'comments' => $comments
             ]);
-
-//        dump($slug,$this, $GLOBALS);
 
         // Dont do that! :D
 //        $html = $twigEnvironment->render('article/show.html.twig',
