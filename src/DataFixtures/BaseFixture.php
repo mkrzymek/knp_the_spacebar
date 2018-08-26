034eb0dc1cc4e2e021b8d9bb63121fb1133640c0
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-08-26 19:13:30 +0200

    Saving ManyToMany relation + joins

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -39,12 +39,13 @@ abstract class BaseFixture extends Fixture
         }
     }
 
-    protected function getRandomReference(string $className) {
+    protected function getRandomReference(string $className)
+    {
         if (!isset($this->referencesIndex[$className])) {
             $this->referencesIndex[$className] = [];
 
             foreach ($this->referenceRepository->getReferences() as $key => $ref) {
-                if (strpos($key, $className.'_') === 0) {
+                if (strpos($key, $className . '_') === 0) {
                     $this->referencesIndex[$className][] = $key;
                 }
             }
@@ -57,5 +58,15 @@ abstract class BaseFixture extends Fixture
         $randomReferenceKey = $this->faker->randomElement($this->referencesIndex[$className]);
 
         return $this->getReference($randomReferenceKey);
+    }
+
+    protected function getRandomReferences(string $className, int $count)
+    {
+        $references = [];
+        while (count($references) < $count) {
+            $references[] = $this->getRandomReference($className);
+        }
+
+        return $references;
     }
 }
